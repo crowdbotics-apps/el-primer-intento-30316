79dc3c6d1524104e09a9d058908c3af4ca4bae8e
Crowdbotics <team@crowdbotics.com>
2021-09-04 18:18:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "el_primer_intento_30316Identifier",
+  url: "https://el_primer_intento_30316Identifier.botics.co",
+  api: "https://el_primer_intento_30316Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
